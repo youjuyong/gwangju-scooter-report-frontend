a0afb9b026f50c8cf6f00f3a009a6f43b9d21db5
youjuyong <dbwndyd518@naver.com>
2026-03-17 14:56:42 +0900

    파이어 베이스 수정 4

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,59 +11,41 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// self.addEventListener("push", function (e) {
-//   console.log("Push received:", e);
+self.addEventListener("push", function (e) {
+  console.log("Push received:", e);
 
-//   let title = "새 알림";
-//   let body = "내용이 없습니다.";
-//   let url = "/";
+  let title = "새 알림";
+  let body = "내용이 없습니다.";
+  let url = "/";
 
-//   if (e.data) {
-//     try {
-//       const payload = e.data.json();
-//       const target = payload.data || payload; 
+  if (e.data) {
+    try {
+      const payload = e.data.json();
+      const target = payload.data || payload; 
       
-//       title = target.title || title;
-//       body = target.body || body;
-//       url = target.url || url;
-//     } catch (err) {
-//       console.error("JSON 파싱 에러:", err);
-//       body = e.data.text();
-//     }
-//   }
+      title = target.title || title;
+      body = target.body || body;
+      url = target.url || url;
+    } catch (err) {
+      console.error("JSON 파싱 에러:", err);
+      body = e.data.text();
+    }
+  }
 
-//   // 데이터 여부와 상관없이 무조건 알림 표시
-//   e.waitUntil(
-//     self.registration.showNotification(title, {
-//       body: body,
-//       data: { url: url },
-//       icon: "/push-icon.png",
-//       badge: "/badge.png",
-//       tag: "pm-report-alert",
-//       renotify: true,
-//       vibrate: [200, 100, 200],
-//     })
-//   );
-// });
-
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] 백그라운드 메시지 수신: ', payload);
-
-  // 서버에서 보낸 notification 또는 data에서 값 추출
-  const title = payload.notification?.title || payload.data?.title || "새 알림";
-  const body = payload.notification?.body || payload.data?.body || "";
-  const url = payload.data?.url || "/"; // 클릭 시 이동할 URL
-
-  return self.registration.showNotification(title, {
-    body: body,
-    icon: "/push-icon.png",
-    badge: "/badge.png",
-    data: { url: url }, // notificationclick에서 쓰기 위함
-    tag: "pm-report-alert",
-    renotify: true,
-  });
+  // 데이터 여부와 상관없이 무조건 알림 표시
+  e.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      data: { url: url },
+      icon: "/push-icon.png",
+      badge: "/badge.png",
+      tag: "pm-report-alert",
+      renotify: true,
+      vibrate: [200, 100, 200],
+    })
+  );
 });
+
 
 /**
  * 알림 클릭 → React 페이지 이동
