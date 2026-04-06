645b8744fd936d37ef237f720fca6ed8ed97cbc3
youjuyong <dbwndyd518@naver.com>
2026-04-06 20:48:17 +0900

    환경변수 수정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  output: 'standalone'
+  output: 'standalone',
+  async rewrites() {
+    const apiUrl = process.env.INTERNAL_API_URL || "http://fac.raontec.co.kr:28085";
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${apiUrl}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
