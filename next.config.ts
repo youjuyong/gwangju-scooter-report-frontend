a7fb0b379771cc1db60359041135a7455845e90c
youjuyong <dbwndyd518@naver.com>
2026-04-08 10:31:49 +0900

    환경설정 수정

--- next.config.ts
+++ next.config.ts
@@ -5,18 +5,11 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   async rewrites() {
     const apiUrl = process.env.INTERNAL_API_URL || "http://fac.raontec.co.kr:28085";
-    const loginApiUrl = process.env.INTERNAL_LOGIN_API_URL || "http://fac.raontec.co.kr:28086";
 
     return [
       {
-        // 일반 API 서버
         source: "/api/:path*",
         destination: `${apiUrl}/api/:path*`,
-      },
-      {
-        // 로그인/인증 서버
-        source: "/login-api/:path*",
-        destination: `${loginApiUrl}/api/:path*`, 
       },
     ];
   },
