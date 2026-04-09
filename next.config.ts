7771e207a3a3687e396798c6458f5cd526c1fba6
youjuyong <dbwndyd518@naver.com>
2026-04-09 14:12:02 +0900

    api 요청 환경설정 수정

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,15 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   async rewrites() {
     const apiUrl = process.env.INTERNAL_API_URL || "http://fac.raontec.co.kr:28085";
+    const authUrl = process.env.NEXT_PUBLIC_LOGIN_API_URL || "https://raonbackend.kr"; 
 
     return [
+      // 1. 인증 관련 API
+      {
+        source: "/api/auth/:path*",
+        destination: `${authUrl}/api/auth/:path*`,
+      },
+      // 2. 그 외 모든 /api 요청
       {
         source: "/api/:path*",
         destination: `${apiUrl}/api/:path*`,
