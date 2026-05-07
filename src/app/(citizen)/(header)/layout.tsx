956f310ff5fbeddbbf27498483ebb419c2231c1a
kchangju <bmlfid519@gmail.com>
2026-05-07 15:57:45 +0900

    신고확인 layout 추가

--- src/app/(citizen)/(header)/layout.tsx
+++ src/app/(citizen)/(header)/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
     let activeTab = "홈";
     if (pathname.includes("/notice")) activeTab = "공지사항";
-    else if (pathname.includes("/report")) activeTab = "신고하기";
+    else if (pathname.includes("/reportList")) activeTab = "신고확인";
 
     // 컴포넌트가 브라우저에 완전히 마운트된 후에만 화면을 보여줌
     const isClient = useSyncExternalStore(
@@ -24,12 +24,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         return <div className="loading-screen"></div>; // 또는 빈 화면
     }
 
+    const isSubPage = pathname.startsWith("/notice") || pathname.startsWith("/reportList");
+
     return (
         <div className={`wrap ${pathname === "/" ? "main-wrap" : "sub-wrap"}`}>
             {/* 이 그룹에 속한 페이지들 상단에만 헤더가 나타납니다 */}
             <Header activeTab={activeTab} setActiveTab={() => {}} />
 
-            <main className={pathname === "/notice" ? "sub_article sub_article_padding" : "main_article"}>
+            <main className={isSubPage ? "sub_article sub_article_padding" : "main_article"}>
                 {children}
             </main>
         </div>
