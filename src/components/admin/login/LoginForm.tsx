2ed772100be99e951b886d1cacf9effff20a393a
youjuyong <dbwndyd518@naver.com>
2026-05-12 10:25:34 +0900

    로그인 분기 추가

--- src/components/admin/login/LoginForm.tsx
+++ src/components/admin/login/LoginForm.tsx
@@ -126,11 +126,9 @@ export default function LoginForm() {
             if (resultCode === "E008") {
                 toast.error(ERROR_MESSAGES[resultCode]);
                 return;
-            }else if(resultCode === "E005") {
-                toast.error(err.response?.data.resultMsg);
             }
 
-            handleApiError(err, "로그인 정보가 올바르지 않습니다.");
+            handleApiError(err, err.response?.data.resultMsg);
         }
     };
 
--- src/services/auth/loginApi.ts
+++ src/services/auth/loginApi.ts
@@ -4,11 +4,11 @@ import {LoginData, LoginResponseData} from "@/types/login";
 
 export const loginService = {
     /*
-    * 서비스 관리자 로그인 API
+    *  pm 로그인 API
     * */
     login: async (data: LoginData): Promise<ApiResponse<LoginResponseData>> => {
         const response = await authApi.post<ApiResponse<LoginResponseData>>(
-            "/admin/login",
+            "/pm/login",
             data
         );
 
