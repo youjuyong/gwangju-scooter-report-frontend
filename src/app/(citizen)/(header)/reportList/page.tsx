99683cfd6e03ca9de79758279d5db875fd2160a0
kchangju <bmlfid519@gmail.com>
2026-05-20 10:14:16 +0900

    신고자 신고확인 로딩바 추가

--- src/app/(citizen)/(header)/reportList/page.tsx
+++ src/app/(citizen)/(header)/reportList/page.tsx
@@ -3,6 +3,7 @@
 import React, {useEffect, useState} from "react";
 import {getReportList} from "@/services/report/reportApi";
 import {useRouter} from "next/navigation";
+import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function ReportListPage() {
     const [reportList, setReportList] = useState<any[]>([]);
@@ -30,62 +31,68 @@ export default function ReportListPage() {
         router.push(`/reportDetail/${id}`);
     };
 
-    if (isLoading) return <div className="loading_box">로딩 중...</div>;
-
     return (
         <article className="subBoard">
             <h2>신고확인</h2>
             <ul className="listBody">
-                {reportList.map((item) => {
-                    const isCompleted = ['DEST04', 'DEST08'].includes(item.dclrStts?.cdId);
-                    const statusClass = isCompleted ? 'si2' : 'si1';
-                    const firstImage = (item.imgUrls && item.imgUrls.length > 0 && item.imgUrls[0].startsWith("data:image"))
-                        ? item.imgUrls[0]
-                        : "/images/camera.png";
+                {isLoading ? (
+                    <LoadingOverlay
+                        message={"데이터를 로딩중입니다.."}
+                    />
+                ) : reportList.length === 0 ? (
+                    <p className="none renone">신고 내역이 없습니다.</p>
+                ) : (
+                    reportList.map((item) => {
+                        const isCompleted = ['DEST04', 'DEST08'].includes(item.dclrStts?.cdId);
+                        const statusClass = isCompleted ? 'si2' : 'si1';
+                        const firstImage = (item.imgUrls && item.imgUrls.length > 0 && item.imgUrls[0].startsWith("data:image"))
+                            ? item.imgUrls[0]
+                            : "/images/camera.png";
 
-                    return (
-                        <li key={item.dclrId} onClick={() => getDetail(item.dclrId)}>
-                            <a>
+                        return (
+                            <li key={item.dclrId} onClick={() => getDetail(item.dclrId)}>
+                                <a>
 
-                                <p className={`situation ${statusClass}`}>
-                                    {item.dclrStts?.cdNm}
-                                </p>
-                                <p className="add">{item.dclrAddrTxt}</p>
+                                    <p className={`situation ${statusClass}`}>
+                                        {item.dclrStts?.cdNm}
+                                    </p>
+                                    <p className="add">{item.dclrAddrTxt}</p>
 
-                                <div className="listconten">
-                                    <div className="leftbox">
-                                        <dl>
-                                            <dt>신고일시</dt>
-                                            <dd>{item.regDt}</dd>
-                                        </dl>
-                                        <dl>
-                                            <dt>킥보드ID</dt>
-                                            <dd>{item.qrcdVl}</dd>
-                                        </dl>
-                                        <dl>
-                                            <dt>위반유형</dt>
-                                            <dd>{item.vltnType?.cdNm}</dd>
-                                        </dl>
-                                        <dl>
-                                            <dt>상세설명</dt>
-                                            <dd>{item.dclrCn}</dd>
-                                        </dl>
-                                        <dl>
-                                            <dt>처리일시</dt>
-                                            <dd className="blue">{item.prcsDt || "-"}</dd>
-                                        </dl>
+                                    <div className="listconten">
+                                        <div className="leftbox">
+                                            <dl>
+                                                <dt>신고일시</dt>
+                                                <dd>{item.regDt}</dd>
+                                            </dl>
+                                            <dl>
+                                                <dt>킥보드ID</dt>
+                                                <dd>{item.qrcdVl}</dd>
+                                            </dl>
+                                            <dl>
+                                                <dt>위반유형</dt>
+                                                <dd>{item.vltnType?.cdNm}</dd>
+                                            </dl>
+                                            <dl>
+                                                <dt>상세설명</dt>
+                                                <dd>{item.dclrCn}</dd>
+                                            </dl>
+                                            <dl>
+                                                <dt>처리일시</dt>
+                                                <dd className="blue">{item.prcsDt || "-"}</dd>
+                                            </dl>
+                                        </div>
+
+                                        <img
+                                            src={firstImage}
+                                            className="list_img"
+                                            alt="신고된이미지"
+                                        />
                                     </div>
-
-                                    <img
-                                        src={firstImage}
-                                        className="list_img"
-                                        alt="신고된이미지"
-                                    />
-                                </div>
-                            </a>
-                        </li>
-                    );
-                })}
+                                </a>
+                            </li>
+                        );
+                    })
+                )}
             </ul>
         </article>
     );
